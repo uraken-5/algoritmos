1e5fbd7ca2aea1509189077f2d59f83158f847d6
Jorge <jorgeeduardoc218@gmail.com>
2024-01-15 01:12:33 -0300

    add image

--- com/jorge/binarysearch/BinarySearch.java
+++ com/jorge/binarysearch/BinarySearch.java
@@ -13,7 +13,14 @@ public class BinarySearch {
         System.out.println("the position are " + position);
     }
 
-
+    /***
+     * Este metodo recursivo, si bien comple su funcion, es elegante y facil de leer tiene un problema.
+     * Cada llamada recursiva añade una nueva capa en la pila de llamadas, lo que puede llevar a
+     * un uso significativo de la memoria para listas muy grandes o búsquedas profundas.
+     * @param list
+     * @param searchingValue
+     * @return
+     */
     public static Integer binarySearch(List<Integer> list, int searchingValue){
 
         if (list.isEmpty()) {return null;}
@@ -30,18 +37,20 @@ public class BinarySearch {
         }
     }
 
-    /*
+    /***
      * En cada iteracion, se corta a la mitad el tamaño de la lista, o sea que cada vez es mas pequeña y la busqueda sera mas corta,
      * incluso si los numeros de la lista crecen.
      * Complejidad Logarítmica (O(log n)): Debido a que el algoritmo divide la lista a la mitad en cada iteración,
      * el número de pasos necesarios para llegar al resultado (o concluir que el valor no está en la lista)
      * crece logarítmicamente con respecto al tamaño de la lista. Esto es lo que llamamos complejidad logarítmica, denotada como O(log n).
+     * @param list
+     * @param searchingValue
+     * @return
+     */
+    /*
+     *
      * */
     public static Integer binarySearch2(List<Integer> list, int searchingValue) {
-        /*
-            Estas inicializaciones no afectan la complejidad ya que se ejecutan
-            solo una vez, independiente del tamaño de la lista. Se consideran O(1), que es constante
-        */
         int low = 0;
         int high = list.size() - 1;
         int step = 0;
@@ -59,6 +68,6 @@ public class BinarySearch {
                 low = mid + 1;
             }
         }
-            return -1;
+        return -1;
     }
 }
